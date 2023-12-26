335266e484ec0737831ff444a8ce9a0b62bf01f9
dulait <draskovicdusan4@gmail.com>
2023-12-26 18:42:55 +0100

    added comments

--- digitalna-prodavnica/routes/api.php
+++ digitalna-prodavnica/routes/api.php
@@ -17,11 +17,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// resource ruta
+Route::middleware('auth:sanctum')->resource('karte', KartaController::class);
+
+// razliciti tipovi ruta; osnovni, wildcard
 Route::get('/karte', [KartaController::class, 'index']);
 Route::get('/karte/{brojKarte}', [KartaController::class, 'show']);
 Route::get('/korisnici', [KorisnikController::class, 'index']);
 Route::get('/korisnici/{id}', [KorisnikController::class, 'show']);
 
+// api rute
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
