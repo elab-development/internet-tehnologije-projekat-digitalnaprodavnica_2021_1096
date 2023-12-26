5467e3251440ab3d1f9351c20d567d901a0b7430
dulait <draskovicdusan4@gmail.com>
2023-12-26 16:24:33 +0100

    fixed api routes & controllers

--- digitalna-prodavnica/routes/api.php
+++ digitalna-prodavnica/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KartaController;
 use App\Http\Controllers\KorisnikController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +20,7 @@ Route::get('/karte', [KartaController::class, 'index']);
 Route::get('/karte/{brojKarte}', [KartaController::class, 'show']);
 Route::get('/korisnici', [KorisnikController::class, 'index']);
 Route::get('/korisnici/{id}', [KorisnikController::class, 'show']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
