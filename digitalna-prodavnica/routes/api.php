eeec5afa63b5d60c1003fdf6f525b20c74900e79
dulait <draskovicdusan4@gmail.com>
2023-12-27 19:46:39 +0100

    configured pagination & filtering

--- digitalna-prodavnica/routes/api.php
+++ digitalna-prodavnica/routes/api.php
@@ -8,12 +8,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/karte', [KartaController::class, 'index']);
 Route::get('/karte/{brojKarte}', [KartaController::class, 'show']);
+Route::get('/karte/kategorija/{tipSporta}', [KartaController::class, 'vratiKartePoSportu']);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/korisnici/{korisnikId}/dodaj-kartu/{brojKarte}', [KartaKorisnikController::class, 'dodajKartu']);
+    Route::post('/korisnici/{korisnikId}/dodaj-kartu/{brojKarte}', [KartaKorisnikController::class, 'kupiKartu']);
     Route::get('/korisnici/{korisnikId}/karte', [KartaKorisnikController::class, 'vratiSveKarte']);
     Route::get('/korisnici/{korisnikId}/karte/{brojKarte}', [KartaKorisnikController::class, 'vratiKartu']);
     Route::get('/korisnici', [KorisnikController::class, 'index']);
