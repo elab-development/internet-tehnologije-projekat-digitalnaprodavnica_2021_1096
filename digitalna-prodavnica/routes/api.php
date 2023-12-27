712ba1f09466216fccbc8a722b72b9f54b57e254
dulait <draskovicdusan4@gmail.com>
2023-12-27 11:09:08 +0100

    removed unnecessary comments & imports

--- digitalna-prodavnica/routes/api.php
+++ digitalna-prodavnica/routes/api.php
@@ -3,19 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KartaController;
 use App\Http\Controllers\KorisnikController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 // resource ruta
 Route::middleware('auth:sanctum')->resource('karte', KartaController::class);
