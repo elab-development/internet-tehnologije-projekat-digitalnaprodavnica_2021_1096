1f1854d296e14050432b69ee46d313fb96bbe40f
dulait <draskovicdusan4@gmail.com>
2024-01-01 22:00:52 +0100

    seeder for the associative table knjiga_autor

---
+++ digitalna-prodavnica/database/seeders/KnjigaAutorSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Autor;
+use App\Models\Knjiga;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class KnjigaAutorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $knjige = Knjiga::all();
+        $autori = Autor::all();
+
+        $knjige->each(function ($knjiga) use ($autori) {
+            $knjiga->autori()->attach(
+                $autori->random(rand(1, 3))->pluck('autor_id')->toArray()
+            );
+        });
+    }
+}
