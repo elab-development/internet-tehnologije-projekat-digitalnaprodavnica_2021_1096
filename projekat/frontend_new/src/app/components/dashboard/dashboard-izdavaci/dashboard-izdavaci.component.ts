1b64d090f4f0ffc63c79802329187d98ffddbace
dulait <draskovicdusan4@gmail.com>
2024-01-15 22:04:24 +0100

    configured CRUD for publishers

--- projekat/frontend_new/src/app/components/dashboard/dashboard-izdavaci/dashboard-izdavaci.component.ts
+++ projekat/frontend_new/src/app/components/dashboard/dashboard-izdavaci/dashboard-izdavaci.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { KorisnikService } from 'src/app/services/korisnik.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { IzdavacService } from 'src/app/services/izdavac.service';
+import { CreateIzdavacComponent } from './create-izdavac/create-izdavac.component';
+import { EditIzdavacComponent } from './edit-izdavac/edit-izdavac.component';
 
 @Component({
   selector: 'app-dashboard-izdavaci',
@@ -6,5 +15,81 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard-izdavaci.component.scss']
 })
 export class DashboardIzdavaciComponent {
+  kolone: string[] = [
+    'izdavac_id',
+    'naziv',
+    'adresa',
+    'akcije',
+  ];
 
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+
+  constructor(private dialog: MatDialog, private izdavacService: IzdavacService, private snackBar: MatSnackBar) { }
+
+  ngOnInit(): void {
+    this.vratiSveIzdavace();
+  }
+
+  ngAfterViewInit(): void {
+    if (this.dataSource) {
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  vratiSveIzdavace() {
+    this.izdavacService.vratiSveIzdavace().subscribe({
+      next: (response) => {
+        console.log(response);
+        this.dataSource = new MatTableDataSource(response.izdavaci);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: console.log,
+    })
+  }
+
+  obrisiIzdavaca(izdavac_id: number) {
+    this.izdavacService.obrisiIzdavaca(izdavac_id).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.snackBar.open(response.status, 'Zatvori', {
+          duration: 3000,
+          horizontalPosition: 'center',
+          verticalPosition: 'bottom',
+        });
+        this.vratiSveIzdavace();
+      }
+    })
+  }
+
+  openCreateIzdavacDialog() {
+    const dialogRef = this.dialog.open(CreateIzdavacComponent);
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.vratiSveIzdavace();
+        }
+      }
+    })
+  }
+
+  openEditIzdavacDialog(podaci: any) {
+    this.dialog.open(EditIzdavacComponent, {
+      data: podaci,
+    });
+  }
 }
