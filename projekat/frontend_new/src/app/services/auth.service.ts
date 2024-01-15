3a0d3cb637cebccf4e2cfdf59d8fed1db69926b7
dulait <draskovicdusan4@gmail.com>
2024-01-15 22:05:14 +0100

    configured reset password logic

--- projekat/frontend_new/src/app/services/auth.service.ts
+++ projekat/frontend_new/src/app/services/auth.service.ts
@@ -47,4 +47,17 @@ export class AuthService {
     }
     return this.http.put<any>(`http://127.0.0.1:8000/api/korisnici/${podaci.korisnik_id}`, podaci, httpOptions);
   }
+
+  posaljiMejl(email: string) {
+    const formData = new FormData(); // nece da radi ako se ne salje kao FormData()
+    formData.append('email', email);
+    return this.http.post<any>("http://127.0.0.1:8000/api/auth/zaboravljena-lozinka", formData);
+  }
+
+  resetujLozinku(password: string) {
+    const token = localStorage.getItem('reset-password-token');
+    const formData = new FormData(); // nece da radi ako se ne salje kao FormData()
+    formData.append('password', password);
+    return this.http.post<any>(`http://127.0.0.1:8000/api/auth/promena-lozinke/${token}`, formData);
+  }
 }
