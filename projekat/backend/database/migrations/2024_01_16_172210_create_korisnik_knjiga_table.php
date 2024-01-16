a0488637b2a1215ce102413feb9daeb66c2ffe51
dulait <draskovicdusan4@gmail.com>
2024-01-16 21:49:56 +0100

    table for books bought by users

---
+++ projekat/backend/database/migrations/2024_01_16_172210_create_korisnik_knjiga_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('korisnik_knjiga', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('korisnik_id');
+            $table->unsignedBigInteger('knjiga_id');
+            $table->timestamps();
+
+            $table->foreign('korisnik_id')
+                ->references('korisnik_id')
+                ->on('korisnik')
+                ->onDelete('cascade');
+            $table->foreign('knjiga_id')
+                ->references('knjiga_id')
+                ->on('knjiga')
+                ->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('korisnik_knjiga');
+    }
+};
