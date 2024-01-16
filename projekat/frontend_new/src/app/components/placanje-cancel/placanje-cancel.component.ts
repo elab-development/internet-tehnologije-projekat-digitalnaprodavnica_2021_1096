ce9080cbc0f5a23c4afbfe8f5318123e1f8f20a0
dulait <draskovicdusan4@gmail.com>
2024-01-16 21:54:17 +0100

    implemented components for Sripe payment api

---
+++ projekat/frontend_new/src/app/components/placanje-cancel/placanje-cancel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-placanje-cancel',
+  templateUrl: './placanje-cancel.component.html',
+  styleUrls: ['./placanje-cancel.component.scss']
+})
+export class PlacanjeCancelComponent {
+
+}
