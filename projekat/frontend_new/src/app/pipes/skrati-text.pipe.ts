9c6d824abd54b5200dcc77184d1634838bc1693c
dulait <draskovicdusan4@gmail.com>
2024-01-17 19:05:54 +0100

    implemented custom pipe that truncates text

---
+++ projekat/frontend_new/src/app/pipes/skrati-text.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: 'truncateText'
+})
+
+export class TruncateTextPipe implements PipeTransform {
+    transform(text: string, maxLength: number = 50) {
+        if (text.length <= maxLength) {
+            return text;
+        } else {
+            return text.substring(0, maxLength) + '...';
+        }
+    }
+}
