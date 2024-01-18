8aed4204857ad87b314918c5d271dd00227975c1
dulait <draskovicdusan4@gmail.com>
2024-01-18 12:13:08 +0100

    added comments

--- projekat/backend/routes/api.php
+++ projekat/backend/routes/api.php
@@ -20,29 +20,32 @@ Route::post('/auth/promena-lozinke/{token}', [AuthController::class, 'promeniLoz
 // api rute -> samo ulogovani korisnici
 Route::middleware('auth:sanctum')->group(function () {
 
+    // api rute -> crud za korisnike
     Route::post('/korisnici', [KorisnikController::class, 'store']); // dodato zbog frontenda
     Route::get('/korisnici/{korisnik_id}', [KorisnikController::class, 'vratiKorisnika']); // dodato zbog frontenda
     Route::get('/{username}/profil', [KorisnikController::class, 'show']);
     Route::get('/korisnici', [KorisnikController::class, 'index']);
     Route::put('/korisnici/{korisnik_id}', [KorisnikController::class, 'update']);
     Route::delete('/korisnici/{korisnik_id}', [KorisnikController::class, 'destroy']);
-
     Route::post('/{korisnik_id}/dodaj-profilnu-sliku', [KorisnikController::class, 'dodajProfilnu']);
 
+    // api rute -> crud za korpu
     Route::get('/{korisnik_id}/korpa', [KorpaController::class, 'index']);
     Route::post('/{korisnik_id}/korpa', [StavkaKorpeController::class, 'store']);
     Route::delete('/{korisnik_id}/korpa/{redni_broj_stavke}', [StavkaKorpeController::class, 'destroy']);
     Route::delete('/{korisnik_id}/korpa', [KorpaController::class, 'destroy']);
 
+    // api rute -> crud za knjige
     Route::post('/knjige', [KnjigaController::class, 'store']);
     Route::put('/knjige/{knjiga_id}', [KnjigaController::class, 'update']);
     Route::delete('/knjige/{knjiga_id}', [KnjigaController::class, 'destroy']);
 
+    // api rute -> placanje koriscenjem Stripe API
     Route::post('/{korisnik_id}/placanje', [PlacanjeController::class, 'placanje']);
     Route::post('/{korisnik_id}/success', [PlacanjeController::class, 'success']);
     Route::post('/{korisnik_id}/cancel', [PlacanjeController::class, 'cancel']);
 
-    // api rute -> placanje koriscenjem Stripe API
+    // api rute -> vracanje knjiga u pdf formatu
     Route::post('/knjiga/{knjiga_id}/dodaj-pdf', [KnjigaController::class, 'dodajPDF']);
     Route::get('/knjiga/{knjiga_id}/preuzmi-pdf', [KnjigaController::class, 'preuzmiPDF']);
 
