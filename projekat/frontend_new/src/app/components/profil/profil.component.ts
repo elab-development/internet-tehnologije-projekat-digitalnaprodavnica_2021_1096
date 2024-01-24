0738866f88586e8478baa4b5202bf1a4e653f472
dulait <draskovicdusan4@gmail.com>
2024-01-25 00:05:27 +0100

    added layout for profile page

--- projekat/frontend_new/src/app/components/profil/profil.component.ts
+++ projekat/frontend_new/src/app/components/profil/profil.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { KorisnikService } from 'src/app/services/korisnik.service';
+
+interface Korisnik {
+  email: string
+  username: string,
+  ime: string,
+  prezime: string
+}
 
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.component.html',
   styleUrls: ['./profil.component.scss']
 })
-export class ProfilComponent {
+export class ProfilComponent implements OnInit {
+
+  korisnik: Korisnik | undefined;
+
+  constructor(private korisnikService: KorisnikService) { }
+
+  ngOnInit(): void {
+    this.vratiDetaljeKorisnika();
+  }
+
+  vratiDetaljeKorisnika() {
+    this.korisnikService.vratiDetaljeKorisnika().subscribe({
+      next: (response) => {
+        console.log(response.korisnik);
+        this.korisnik = response.korisnik;
+      }
+    })
+  }
+
+  pokreniIzmenu() {
+
+  }
 
 }
