411cf8ffeb2bf3a0de7749524123e32772aa70af
dulait <draskovicdusan4@gmail.com>
2024-01-25 00:06:01 +0100

    added method that returns user details

--- projekat/frontend_new/src/app/services/korisnik.service.ts
+++ projekat/frontend_new/src/app/services/korisnik.service.ts
@@ -18,6 +18,17 @@ export class KorisnikService {
     return this.http.get<any>("http://127.0.0.1:8000/api/korisnici", httpOptions);
   }
 
+  vratiDetaljeKorisnika() {
+    const korisnik_id = localStorage.getItem('korisnikID');
+    const token = localStorage.getItem('token');
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': `Bearer ${token}`,
+      })
+    }
+    return this.http.get<any>(`http://127.0.0.1:8000/api/korisnici/${korisnik_id}`, httpOptions);
+  }
+
   obrisiKorisnika(korisnikId: number) {
     const token = localStorage.getItem('token');
     const httpOptions = {
