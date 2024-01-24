7993ad1d638c4a44a82384810ca0546d3fdeee06
dulait <draskovicdusan4@gmail.com>
2024-01-25 00:03:31 +0100

    removed web routes

--- projekat/backend/routes/web.php
+++ projekat/backend/routes/web.php
@@ -12,11 +12,3 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/login', function () {
-    return view('login');
-});
