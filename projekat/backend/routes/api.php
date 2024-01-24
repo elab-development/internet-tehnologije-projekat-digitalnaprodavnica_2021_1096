b1ebd35f2f6f131df467b1153b8809f5ec1cbbf9
dulait <draskovicdusan4@gmail.com>
2024-01-25 00:03:25 +0100

    new routes for statistics

--- projekat/backend/routes/api.php
+++ projekat/backend/routes/api.php
@@ -10,23 +10,40 @@ use App\Http\Controllers\PlacanjeController;
 use App\Http\Controllers\StavkaKorpeController;
 use Illuminate\Support\Facades\Route;
 
-
 // api rute -> login, register, zaboravljenu lozinku i promenu lozinke
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/zaboravljena-lozinka', [AuthController::class, 'zaboravljenaLozinka']);
 Route::post('/auth/promena-lozinke/{token}', [AuthController::class, 'promeniLozinku']);
 
-// api rute -> samo ulogovani korisnici
+// api rute -> knjige
+Route::get('/knjige', [KnjigaController::class, 'index']);
+Route::get('/knjige/{knjiga_id}', [KnjigaController::class, 'show']);
+Route::get('/knjige/kategorija/{kategorija}', [KnjigaController::class, 'vratiKnjigeUKategoriji']);
+
+// api ruta -> vraca sve kupljene knjige
+Route::get('/{korisnik_id}/moje-knjige', [KorisnikController::class, 'vratiKupljeneKnjige']);
+
+// api rute -> prikaz knjiga od autora i izdavaca
+Route::get('/knjige/autor/{autor_id}', [AutorController::class, 'vratiKnjigeAutora']);
+Route::get('/knjige/izdavac/{izdavac_id}', [IzdavacController::class, 'vratiKnjigeIzdavaca']);
+
+// api rute -> vizuelizacija na frontendu
+Route::get('/broj-knjiga-po-kategoriji', [KnjigaController::class, 'vratiBrojKnjigaPoKategoriji']);
+Route::get('/broj-kupljenih-knjiga-po-kategoriji', [KnjigaController::class, 'vratiBrojKupljenihKnjigaPoKategoriji']);
+Route::get('/prodaja-tokom-vremena', [KnjigaController::class, 'vratiProdajuKnjigaTokomVremena']);
+
+// api rute -> samo autentifikovani korisnici
 Route::middleware('auth:sanctum')->group(function () {
 
     // api rute -> crud za korisnike
-    Route::post('/korisnici', [KorisnikController::class, 'store']); // dodato zbog frontenda
-    Route::get('/korisnici/{korisnik_id}', [KorisnikController::class, 'vratiKorisnika']); // dodato zbog frontenda
-    Route::get('/{username}/profil', [KorisnikController::class, 'show']);
+    Route::post('/korisnici', [KorisnikController::class, 'store']);
+    Route::get('/korisnici/{korisnik_id}', [KorisnikController::class, 'show']);
     Route::get('/korisnici', [KorisnikController::class, 'index']);
     Route::put('/korisnici/{korisnik_id}', [KorisnikController::class, 'update']);
     Route::delete('/korisnici/{korisnik_id}', [KorisnikController::class, 'destroy']);
+
+    // api ruta -> dodavanje profilne slike
     Route::post('/{korisnik_id}/dodaj-profilnu-sliku', [KorisnikController::class, 'dodajProfilnu']);
 
     // api rute -> crud za korpu
@@ -55,18 +72,3 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/auth/logout', [AuthController::class, 'logout']);
 });
-
-// api rute -> knjige
-Route::get('/knjige', [KnjigaController::class, 'index']);
-Route::get('/knjige/{knjiga_id}', [KnjigaController::class, 'show']);
-Route::get('/knjige/kategorija/{kategorija}', [KnjigaController::class, 'vratiKnjigeUKategoriji']);
-
-
-// api ruta -> vraca sve kupljene knjige
-Route::get('/{korisnik_id}/moje-knjige', [KorisnikController::class, 'vratiKupljeneKnjige']);
-
-// api rute -> prikaz knjiga od autora i izdavaca
-Route::get('/knjige/autor/{autor_id}', [AutorController::class, 'vratiKnjigeAutora']);
-Route::get('/knjige/izdavac/{izdavac_id}', [IzdavacController::class, 'vratiKnjigeIzdavaca']); 
-
-// todo -> testiranje ruta
